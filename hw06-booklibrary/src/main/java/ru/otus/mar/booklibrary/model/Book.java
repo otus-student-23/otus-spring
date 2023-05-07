12e31b48f0e01ab4a0a07c336f9aa2a366cae65e
Marat Nasyrov <m.nasyrov@open.ru>
2023-05-08 00:15:55 +0500

    hw06-booklibrary-orm

--- hw06-booklibrary/src/main/java/ru/otus/mar/booklibrary/model/Book.java
+++ hw06-booklibrary/src/main/java/ru/otus/mar/booklibrary/model/Book.java
@@ -1,6 +1,5 @@
 package ru.otus.mar.booklibrary.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
