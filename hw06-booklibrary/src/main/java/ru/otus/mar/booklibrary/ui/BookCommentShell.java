2216ae94e90f15225122a9eb86c50cad2d256cf9
Marat Nasyrov <m.nasyrov@open.ru>
2023-05-08 19:20:44 +0500

    добавлена возможность редактирования и удаления комментария

--- hw06-booklibrary/src/main/java/ru/otus/mar/booklibrary/ui/BookCommentShell.java
+++ hw06-booklibrary/src/main/java/ru/otus/mar/booklibrary/ui/BookCommentShell.java
@@ -33,7 +33,36 @@ public class BookCommentShell {
                 .toList();
     }
 
+    @ShellMethodAvailability(value = "isBookSelected")
+    @ShellMethod(value = "get selected book comment", key = {"g c", "get comment"})
+    public BookCommentDto add(@ShellOption(value = {"-n", "--number"}) int num) {
+        //TODO оптимизировать поиск комментария
+        BookCommentDto comment = service.getByBook((BookDto) mainShell.getSelectedEntity()).get(num);
+        mainShell.selectEntity(comment, comment.comment());
+        return comment;
+    }
+
+    @ShellMethodAvailability(value = "isEntitySelected")
+    @ShellMethod(value = "update selected comment", key = {"u c", "update comment"})
+    public BookCommentDto update(@ShellOption(value = {"-c", "--comment"}) String comment) {
+        BookCommentDto selComment = (BookCommentDto) mainShell.getSelectedEntity();
+        BookCommentDto updComment = service.update(new BookCommentDto(selComment.id(), selComment.book(), comment));
+        mainShell.selectEntity(updComment, updComment.comment());
+        return updComment;
+    }
+
+    @ShellMethodAvailability(value = "isEntitySelected")
+    @ShellMethod(value = "delete selected comment", key = {"d c", "delete comment"})
+    public void delete() {
+        service.delete((BookCommentDto) mainShell.getSelectedEntity());
+        mainShell.reset();
+    }
+
     private Availability isBookSelected() {
         return mainShell.isEntityClassSelected(BookDto.class);
+    }
+
+    private Availability isEntitySelected() {
+        return mainShell.isEntityClassSelected(BookCommentDto.class);
     }
 }
