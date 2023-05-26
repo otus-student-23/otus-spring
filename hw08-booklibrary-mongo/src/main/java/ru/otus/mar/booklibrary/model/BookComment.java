0add36ca6faef697ae610604418ac848b1d9f6d1
Marat Nasyrov <m.nasyrov@open.ru>
2023-05-27 00:03:51 +0500

    мелкие правки

--- hw08-booklibrary-mongo/src/main/java/ru/otus/mar/booklibrary/model/BookComment.java
+++ hw08-booklibrary-mongo/src/main/java/ru/otus/mar/booklibrary/model/BookComment.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -17,6 +18,7 @@ public class BookComment {
     private String id;
 
     @DBRef
+    @Indexed
     private Book book;
 
     private String comment;
