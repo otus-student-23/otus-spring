1572cd77eeca37cdd97c93921178d2fce06fb6ee
Marat Nasyrov <m.nasyrov@open.ru>
2023-09-03 19:43:13 +0500

    add dev-profile

--- hw18-docker-k8s/booklibrary-microservice/src/main/java/ru/otus/mar/booklibrary/BookLibraryApplication.java
+++ hw18-docker-k8s/booklibrary-microservice/src/main/java/ru/otus/mar/booklibrary/BookLibraryApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class BookLibraryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(BookLibraryApplication.class, args);
     }
 }
